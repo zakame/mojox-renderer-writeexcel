3a0153a1d55497a15555e7f368d148080f87c7d3
Zak B. Elep <zakame@zakame.net>
2010-08-19 14:28:15 +0800

    Bump module version
    To 0.04!
    
    Signed-off-by: Zak B. Elep <zakame@zakame.net>

--- lib/MojoX/Renderer/WriteExcel.pm
+++ lib/MojoX/Renderer/WriteExcel.pm
@@ -5,7 +5,7 @@ use strict;
 
 use Spreadsheet::WriteExcel::Simple;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 # Fry: Why would a robot need to drink?
 # Bender: I don't need to drink. I can quit anytime I want!
